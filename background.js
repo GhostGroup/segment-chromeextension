9828a4a194c7498c1f9d5260ea3407cd0990d9e2
Ho-Fan Kang <me@hofan41.com>
2020-09-25 11:05:22 -0700

    Updated to use weedmaps segment url's

--- background.js
+++ background.js
@@ -1,101 +1,102 @@
 var trackedEvents = new Array();
 
 function zeroPad(i) {
-	if (i < 10) {
-		i = "0" + i
-	}
-	return i;
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
 }
 
-function updateTrackedEventsForTab(tabId,port) {
-	var sendEvents = [];
-	
-	for(var i=0;i<trackedEvents.length;i++) {
-		if (trackedEvents[i].tabId == tabId) {
-			sendEvents.push(trackedEvents[i]);
-		}
-	}
-	
-	port.postMessage({
-		type: 'update',
-		events: sendEvents
-	});
+function updateTrackedEventsForTab(tabId, port) {
+  var sendEvents = [];
+
+  for (var i = 0; i < trackedEvents.length; i++) {
+    if (trackedEvents[i].tabId == tabId) {
+      sendEvents.push(trackedEvents[i]);
+    }
+  }
+
+  port.postMessage({
+    type: "update",
+    events: sendEvents,
+  });
 }
-function clearTrackedEventsForTab(tabId,port) {
-	var newTrackedEvents = [];			
-	for(var i=0;i<trackedEvents.length;i++) {
-		if (trackedEvents[i].tabId != tabId) {
-			newTrackedEvents.push(trackedEvents[i]);
-		}
-	}
-	trackedEvents = newTrackedEvents;
+function clearTrackedEventsForTab(tabId, port) {
+  var newTrackedEvents = [];
+  for (var i = 0; i < trackedEvents.length; i++) {
+    if (trackedEvents[i].tabId != tabId) {
+      newTrackedEvents.push(trackedEvents[i]);
+    }
+  }
+  trackedEvents = newTrackedEvents;
 }
 
 chrome.extension.onConnect.addListener((port) => {
-	port.onMessage.addListener((msg) => {
-		var tabId = msg.tabId;
-		if (msg.type == 'update') {
-			updateTrackedEventsForTab(tabId,port);
-		}
-		else if (msg.type == 'clear') {
-			clearTrackedEventsForTab(tabId,port);
-			updateTrackedEventsForTab(tabId,port);
-		}
-	});
+  port.onMessage.addListener((msg) => {
+    var tabId = msg.tabId;
+    if (msg.type == "update") {
+      updateTrackedEventsForTab(tabId, port);
+    } else if (msg.type == "clear") {
+      clearTrackedEventsForTab(tabId, port);
+      updateTrackedEventsForTab(tabId, port);
+    }
+  });
 });
 
 chrome.webRequest.onBeforeRequest.addListener(
-	(details) => {
-		if (details.url.startsWith('https://api.segment.io/v1')) {
-			var postedString = String.fromCharCode.apply(null,new Uint8Array(details.requestBody.raw[0].bytes));
-			
-			var rawEvent = JSON.parse(postedString);
-			
-			var today = new Date();
-			
-			var h = zeroPad(today.getHours());
-			var m = zeroPad(today.getMinutes());
-			var s = zeroPad(today.getSeconds());
+  (details) => {
+    if (details.url.startsWith("https://api.segment.weedmaps.com/v1")) {
+      var postedString = String.fromCharCode.apply(
+        null,
+        new Uint8Array(details.requestBody.raw[0].bytes)
+      );
 
-			var event = {
-				eventName: rawEvent.event,
-				raw: postedString,
-				trackedTime: h + ':' + m + ':' + s,
-			};
+      var rawEvent = JSON.parse(postedString);
 
-			chrome.tabs.query({
-				active: true,
-				currentWindow: true
-			}, (tabs) => {
-				var tab = tabs[0];
-				
-				event.hostName = tab.url;
-				event.tabId = tab.id;
+      var today = new Date();
 
-				if (details.url == 'https://api.segment.io/v1/t') {
-					event.type = 'track';
-					
-					trackedEvents.unshift(event);
-				}
-				else if (details.url == 'https://api.segment.io/v1/i') {
-					event.eventName = 'Identify';
-					event.type = 'identify';
-					
-					trackedEvents.unshift(event);
-				}
-				else if (details.url == 'https://api.segment.io/v1/p') {
-					event.eventName = 'Page loaded';
-					event.type = 'pageLoad';
-					
-					trackedEvents.unshift(event);
-				}
-			});
-		}
-	},
-	{
-	urls: [
-		"https://*/*",
-		"http://*/*"
-	]},
-	['blocking','requestBody']
+      var h = zeroPad(today.getHours());
+      var m = zeroPad(today.getMinutes());
+      var s = zeroPad(today.getSeconds());
+
+      var event = {
+        eventName: rawEvent.event,
+        raw: postedString,
+        trackedTime: h + ":" + m + ":" + s,
+      };
+
+      chrome.tabs.query(
+        {
+          active: true,
+          currentWindow: true,
+        },
+        (tabs) => {
+          var tab = tabs[0];
+
+          event.hostName = tab.url;
+          event.tabId = tab.id;
+
+          if (details.url == "https://api.segment.weedmaps.com/v1/t") {
+            event.type = "track";
+
+            trackedEvents.unshift(event);
+          } else if (details.url == "https://api.segment.weedmaps.com/v1/i") {
+            event.eventName = "Identify";
+            event.type = "identify";
+
+            trackedEvents.unshift(event);
+          } else if (details.url == "https://api.segment.weedmaps.com/v1/p") {
+            event.eventName = "Page loaded";
+            event.type = "pageLoad";
+
+            trackedEvents.unshift(event);
+          }
+        }
+      );
+    }
+  },
+  {
+    urls: ["https://*/*", "http://*/*"],
+  },
+  ["blocking", "requestBody"]
 );
